80d632e895e8d6f7a4c80e596408b3d48aeab346
Caz Gabrielse <cgabrielse@outlook.com>
2021-03-08 03:09:06 -0600

    Cleaned comments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 /******************************************
 Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
+Project 1 - A Random Quote Generator
 ******************************************/
-
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * An array of quote objects with various data related to each quote
